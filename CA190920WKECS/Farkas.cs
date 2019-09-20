a210a616ba0cfc223336c57f322d358b650fcdc9
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2019-09-20 17:27:02 +0200

    Add project files.

---
+++ CA190920WKECS/Farkas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA190920WKECS
+{
+    class Farkas
+    {
+        public string Nev { get; set; }
+        public bool Farok { get; set; }
+        public int Lab { get; set; }
+        public Farkas(string n, bool f, int l)
+        {
+            Nev = n;
+            Farok = f;
+            Lab = l;
+        }
+    }
+}
---
+++ CA190920WKECS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA190920WKECS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var farkasok = new List<Farkas>()
+            {
+                new Farkas("HosszúFaSzú", true, 5),
+                new Farkas("Hagyma", false, 4),
+                new Farkas("FarkasÚr", true, 4)
+            };
+            Console.WriteLine("Összes farkas neve: ");
+            foreach (var f  in farkasok)
+            {
+                Console.WriteLine(f.Nev);
+            }
+            Console.ReadKey();
+        }
+    }
+}
